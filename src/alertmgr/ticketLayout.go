1b579969669ed5148f6071c6d970327831beb753
Alim Fedoseev <amfedoseyev@gmail.com>
2020-05-11 20:32:39 +0600

    [issue-4] Add ScanImageInfo struct, parser and tests for it.

---
+++ src/alertmgr/ticketLayout.go
@@ -0,0 +1,14 @@
+package alertmgr
+
+import "encoding/json"
+
+
+func ParseImageInfo(source []byte) (*ScanImageInfo, error) {
+	scanInfo := new(ScanImageInfo)
+	err := json.Unmarshal(source, scanInfo)
+	if err != nil {
+		return nil, err
+	}
+	return scanInfo, nil
+}
+
