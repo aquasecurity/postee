cb3a17f5c43c7f1270dc402eb90e3ba78e4c0c28
Alim Fedoseev <amfedoseyev@gmail.com>
2020-05-14 21:42:42 +0600

    [issue-4] Few fixes in ticket layout

--- src/alertmgr/ticketLayout.go
+++ src/alertmgr/ticketLayout.go
@@ -9,21 +9,18 @@ import (
 func GenTicketDescription(scanInfo, prevScan *data.ScanImageInfo) string {
 	var builder bytes.Buffer
 	builder.WriteString("Image name: " + scanInfo.Image + "\n")
-	builder.WriteString("Registry: " + scanInfo.Registry )
+	builder.WriteString("Registry: " + scanInfo.Registry + "\n" )
 	if scanInfo.Disallowed {
 		builder.WriteString("Image is non-compliant\n")
 	} else {
-		builder.WriteString("Image is compliance\n")
+		builder.WriteString("Image is compliant\n")
 	}
-	builder.WriteString(
-		RenderVulnerabilitiesCounts(
-			scanInfo.Critical, scanInfo.High, scanInfo.Medium, scanInfo.Low, scanInfo.Negligible ))
 
 	if scanInfo.ScanMalware {
 		if scanInfo.Malware > 0 {
 			builder.WriteString("Malware found: YES\n")
 		} else {
-			builder.WriteString("Malware found: no\n")
+			builder.WriteString("Malware found: No\n")
 		}
 	}
 
@@ -31,18 +28,24 @@ func GenTicketDescription(scanInfo, prevScan *data.ScanImageInfo) string {
 		if scanInfo.Sensitive > 0 {
 			builder.WriteString("Sensitive data found: yes\n")
 		} else {
-			builder.WriteString("Sensitive data found: no\n")
+			builder.WriteString("Sensitive data found: No\n")
 		}
 	}
+
+	builder.WriteString(
+		RenderVulnerabilitiesCounts(
+			scanInfo.Critical, scanInfo.High, scanInfo.Medium, scanInfo.Low, scanInfo.Negligible ))
 
 	// Rendering Assurances
 	builder.WriteString( RenderAssurances(scanInfo.ImageAssuranceResults))
 
 	// Rendering Found vulnerabilities
-	builder.WriteString( "\nh2. Found vulnerabilities\n")
-	for _, r := range scanInfo.Resources {
-		v := RenderVulnerabilities( r.Name, r.Vulnerabilities)
-		builder.WriteString( v )
+	if len(scanInfo.Resources) > 0 {
+		builder.WriteString( "\nh2. Found vulnerabilities\n")
+		for _, r := range scanInfo.Resources {
+			v := RenderVulnerabilities( r.Name, r.Vulnerabilities)
+			builder.WriteString( v )
+		}
 	}
 
 	// Discovered vulnerabilities from last scan:
