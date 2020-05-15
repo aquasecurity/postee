18fe7d9461d5a1f07053c87038cfc85962eee78c
afdesk <work@afdesk.com>
2020-05-15 16:51:16 +0600

    Issue 4 (#12)
    * Some init changes
    
    * Remove tcontainer from the project
    
    * Remove tcontainer from the project
    
    * Remove run script
    
    * [issue-4] Add ScanImageInfo struct, parser and tests for it.
    
    * [issue-4] Add test and cover to makefile
    
    * [issue-4] Add Jira ticket layout
    
    * [issue-4] Transfer types to the package
    
    * [issue-4] Add db bbolt handling
    
    * [issue-4] Add prev scans handling
    
    * [issue-4] Edit tests
    
    * Update makefile
    
    * Create Dockerfile
    
    * Update webhooksrv.go
    
    * Update Dockerfile
    
    * Update cfg.yaml
    
    * Update cfg.yaml
    
    * [issue-4] Add TestGenTicketDescription
    
    * [issue-4] Fix RenderVulnerabilitiesCounts
    
    * [issue-4] Add dbservice tests
    
    * [issue-4] Add dbservice test to makefile
    
    * [issue-4] Few fixes in ticket layout
    
    * [issue-4] Add bbolt package and test results to .gitignore
    
    Co-authored-by: Alim Fedoseev <amfedoseyev@gmail.com>
    Co-authored-by: jerbia <jerbia@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM golang:alpine as builder
+RUN apk add --update make git
+ADD . /webhook/
+WORKDIR /webhook/
+ENV GOPATH=$GOPATH:/webhook
+RUN make all
+
+
+FROM alpine
+RUN apk update && apk add wget ca-certificates
+EXPOSE 8082
+EXPOSE 8445
+RUN mkdir /server
+RUN mkdir /config
+COPY --from=builder /webhook/bin/webhooksrv /server/
+COPY --from=builder /webhook/cfg.yaml /config/
+WORKDIR /server
+RUN chmod +x webhooksrv
+RUN adduser -D -g '' webhook
+RUN chown -R webhook:webhook /server
+RUN chown -R webhook:webhook /config
+USER webhook
+ENTRYPOINT ["/server/webhooksrv"]
---
+++ src/data/types.go
@@ -0,0 +1,63 @@
+package data
+
+import "fmt"
+
+type ScanImageInfo struct {
+	Image                 string `json:"image"`
+	Registry              string `json:"registry"`
+	Digest                string `json:"digest"`
+	PreviousDigest        string `json:"previous_digest"`
+	ImageAssuranceResults `json:"image_assurance_results"`
+	VulnerabilitySummary  `json:"vulnerability_summary"`
+	ScanOptions           `json:"scan_options"`
+	Resources             []InfoResources `json:"resources"`
+}
+
+type ImageAssuranceResults struct {
+	Disallowed      bool           `json:"disallowed"`
+	ChecksPerformed []ControlCheck `json:"checks_performed"`
+}
+
+type ControlCheck struct {
+	Control    string `json:"control"`
+	PolicyName string `json:"policy_name"`
+	Failed     bool   `json:"failed"`
+}
+
+type ScanOptions struct {
+	ScanSensitiveData bool `json:"scan_sensitive_data"`
+	ScanMalware       bool `json:"scan_malware"`
+}
+
+type VulnerabilitySummary struct {
+	Total      int `json:"total"`
+	Critical   int `json:"critical"`
+	High       int `json:"high"`
+	Medium     int `json:"medium"`
+	Low        int `json:"low"`
+	Negligible int `json:"negligible"`
+	Sensitive  int `json:"sensitive"`
+	Malware    int `json:"malware"`
+}
+
+type InfoResources struct {
+	Vulnerabilities []Vulnerability `json:"vulnerabilities"`
+	ResourceDetails `json:"resource"`
+}
+type ResourceDetails struct {
+	Name string `json:"name"`
+}
+
+type Vulnerability struct {
+	Name       string `json:"name"`
+	Version    string `json:"version"`
+	FixVersion string `json:"fix_version"`
+}
+
+func BuildUniqueId(digest, image, registry string) string {
+	return fmt.Sprintf("%s-%s-%s", digest, image, registry)
+}
+
+func (si *ScanImageInfo) GetUniqueId() string {
+	return BuildUniqueId(si.Digest, si.Image, si.Registry)
+}
