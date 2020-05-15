bd56907d43045a8fa5935610b0a09c27d07538fb
Alim Fedoseev <amfedoseyev@gmail.com>
2020-05-15 22:57:47 +0600

    [issue-4] Fix select from db

--- src/dbservice/select.go
+++ src/dbservice/select.go
@@ -1,11 +1,17 @@
 package dbservice
 
-import bolt "go.etcd.io/bbolt"
+import (
+	bolt "go.etcd.io/bbolt"
+)
 
 func dbSelect(db *bolt.DB,key string) (result []byte, err error) {
 	err = db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(BucketName))
-		result = b.Get([]byte(key))
+		r := b.Get([]byte(key))
+		if r != nil {
+			result = make([]byte, len(r))
+			copy(result, r)
+		}
 		return nil
 	})
 	return
