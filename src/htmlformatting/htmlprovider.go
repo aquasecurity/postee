9cadd6c0bc9a0e91b3a62a7c590fbcd53729032b
Alim Fedoseev <amfedoseyev@gmail.com>
2020-05-16 22:23:45 +0600

    [issue-14] Add provider for html rendering

---
+++ src/htmlformatting/htmlprovider.go
@@ -0,0 +1,44 @@
+package htmlformatting
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+type HtmlProvider struct{}
+
+func (html *HtmlProvider) TitleH2(title string) string {
+	return fmt.Sprintf("<h2>%s</h2>\n", title)
+}
+
+func (html *HtmlProvider) TitleH3(title string) string {
+	return fmt.Sprintf("<h3>%s</h3>\n", title)
+}
+
+func (html *HtmlProvider) ColourText(text, color string) string {
+	return fmt.Sprintf("<span style='color:%s'>%s</span>", color, text)
+}
+
+func (html *HtmlProvider) Table(rows [][]string) string {
+	table := make([]string, 0)
+	table = append(table, "<TABLE>")
+	for i, r := range rows {
+		var tag string
+		if i == 0 {
+			tag = "TH"
+		} else {
+			tag = "TD"
+		}
+		table = append(table, "<TR>")
+		var rowBuilder bytes.Buffer
+		for _, field := range r {
+			rowBuilder.WriteString( fmt.Sprintf("<%s>%s</%s>", tag,field, tag) )
+		}
+		table = append(table, rowBuilder.String())
+		table = append(table, "</TR>")
+	}
+
+	table = append(table, "</TABLE>\n")
+	return strings.Join(table, "\n")
+}
