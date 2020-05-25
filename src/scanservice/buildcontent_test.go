7b7e8c6317184ae216a9cd46d13abb6fdad2b2e0
Alim Fedoseev <amfedoseyev@gmail.com>
2020-05-25 17:57:05 +0600

    [issue-8] Add tests of content building

---
+++ src/scanservice/buildcontent_test.go
@@ -0,0 +1,63 @@
+package scanservice
+
+import (
+	"formatting"
+	"strings"
+	"testing"
+)
+
+var (
+	scan1 = map[string]string{"title":"title1", "description":"<p>description1</p>\n", "name":"name1",}
+	scan2 = map[string]string{"title":"title2", "description":"<p>description2</p>\n", "name":"name2",}
+	lay = new(formatting.HtmlProvider)
+)
+
+func TestBuildAggregatedContent(t *testing.T)  {
+	wantBody := `<h1>title1</h1>
+<p>description1</p>
+<h1>title2</h1>
+<p>description2</p>
+`
+	wantTitle := "name1, name2"
+
+	result := buildAggregatedContent([]map[string]string{scan1, scan2}, lay)
+	if !strings.HasPrefix(result["title"], wantTitle) {
+		t.Errorf("Wrong Title don't contain names\nResult: %q\nWaited: %q", result["title"], wantTitle)
+	}
+
+	if result["description"] != wantBody {
+		t.Errorf("Wrong Description\nResult: %q\nWaited: %q", result["description"], wantBody)
+	}
+}
+
+func TestBuildMapContent(t *testing.T) {
+	tests := [...]struct{
+		title string
+		descr string
+		name  string
+		want  map[string]string
+	}{
+		{
+			"title1", "description1", "name1",
+			scan1,
+		},
+		{
+			"title2", "description2", "name2",
+			scan2,
+		},
+	}
+
+	for _, test := range tests {
+		result := buildMapContent(test.title, lay.P(test.descr), test.name)
+		if len(result) != len(test.want) {
+			t.Errorf("Wrong result size\nResult: %v\nWaited: %v", result, test.want)
+			continue
+		}
+		for k, v := range result {
+			if test.want[k] != v {
+				t.Errorf("Wrong title\nResult: %q\nWaited: %q", v, test.want[k])
+			}
+		}
+	}
+}
+
