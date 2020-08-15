074f4ba46177ae536d79ad529a7f42234d55bc07
afdesk <work@afdesk.com>
2020-08-16 01:22:07 +0600

    Issue 45 (#47)
    * Some init changes
    
    * Remove tcontainer from the project
    
    * [Fix] Add a h1 function for slack
    
    * [issue-45] Add a base for Max_DB_Size and Delete_Old_Data settings
    
    * [issue-45] Fix a getting of bucket from db
    
    * [issue-45] Add TestDbDelete and TestWrongBuckets
    
    * [issue-45] Add a ticker for db tests
    
    * [issue-45] Add error tests for empty bucket names
    
    * [issue-45] Fix ticker
    
    * [issue-45] Add test for email without password
    
    * [issue-45] Add Max_DB_Size and Delete_Old_Data tests
    
    * [issue-45] Add during
    
    * [issue-45] Change test for load serviceNow (using of Default table)
    
    * [issue-45] Change handling of access error to db
    
    * [issue-45] Add TestWithoutAccessToDb
    
    Co-authored-by: Alim Fedoseev <amfedoseyev@gmail.com>

--- src/dbservice/dbparam.go
+++ src/dbservice/dbparam.go
@@ -1,13 +1,25 @@
 package dbservice
 
-import "sync"
-
-const (
-	dbBucketName       = "WebhookBucket"
-	dbBucketAggregator = "WebhookAggregator"
+import (
+	"sync"
+	"time"
 )
 
 var (
+	dbBucketName       = "WebhookBucket"
+	dbBucketAggregator = "WebhookAggregator"
+	dbBucketExpiryDates= "WebookExpiryDates"
+
+	DbSizeLimit = 0
+	DbDueDate   = 0
+	dueTimeBase = time.Hour*time.Duration(24)
+
 	DbPath = "/server/database/webhooks.db"
 	mutex   sync.Mutex
 )
+
+func ChangeDbPath(newPath string) {
+	mutex.Lock()
+	DbPath = newPath
+	mutex.Unlock()
+}
