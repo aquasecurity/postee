f7fe0b3baa0d83f35703168e713a9f3d36fd6c7c
AMF <work@afdesk.com>
2021-04-26 17:38:59 +0600

    [v2] Changes to single config file

--- alertmgr/tenants.go
+++ alertmgr/tenants.go
@@ -1,7 +1,6 @@
 package alertmgr
 
 type TenantSettings struct {
-	Name            string           `json:"name"`
 	AquaServer      string           `json:"AquaServer"`
 	DBMaxSize       int              `json:"Max_DB_Size"`
 	DBRemoveOldData int              `json:"Delete_Old_Data"`
