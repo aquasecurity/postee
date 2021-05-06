2358c5fa7eebfcbec605b0fc85913ab1fd67704b
Andrey Levchenko <levchenko.andrey@gmail.com>
2021-05-06 17:02:57 +0600

    [v2] added some field layout improvements

--- ui/frontend/src/components/PluginDetails.vue
+++ ui/frontend/src/components/PluginDetails.vue
@@ -29,35 +29,41 @@
     <div class="card">
       <form @submit.prevent="doSubmit">
         <div class="card-body">
-          <div class="form-group form-input">
-            <label for="pluginType">Type</label>
-            <select
-              class="form-select form-control"
-              :value="formValues.type"
-              id="pluginType"
-              name="type"
-              @input="updateIntegrationType"
-            >
-              <option value="email">Email</option>
-              <option value="jira">Jira</option>
-              <option value="slack">Slack</option>
-              <option value="teams">Teams</option>
-              <option value="webhook">Webhook</option>
-              <option value="splunk">Splunk</option>
-              <option value="serviceNow">ServiceNow</option>
-            </select>
-            <small id="aHelp" class="form-text text-muted"
-              >The integration type</small
-            >
+          <div class="row">
+            <div class="col">
+              <div class="form-group form-input">
+                <label for="pluginType">Type</label>
+                <select
+                  class="form-select form-control"
+                  :value="formValues.type"
+                  id="pluginType"
+                  name="type"
+                  @input="updateIntegrationType"
+                >
+                  <option value="email">Email</option>
+                  <option value="jira">Jira</option>
+                  <option value="slack">Slack</option>
+                  <option value="teams">Teams</option>
+                  <option value="webhook">Webhook</option>
+                  <option value="splunk">Splunk</option>
+                  <option value="serviceNow">ServiceNow</option>
+                </select>
+                <small id="aHelp" class="form-text text-muted"
+                  >The integration type</small
+                >
+              </div>
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="name"
+                label="Name"
+                :value="formValues.name"
+                :errorMsg="errors['name']"
+                :inputHandler="updateField"
+                :validator="v(uniqueName)"
+              />
+            </div>
           </div>
-          <PluginProperty
-            id="name"
-            label="Name"
-            :value="formValues.name"
-            :errorMsg="errors['name']"
-            :inputHandler="updateField"
-            :validator="v(uniqueName)"
-          />
           <!-- enable is not required here-->
           <PluginCheckboxProperty
             id="enable"
@@ -65,27 +71,33 @@
             :value="formValues.enable"
             :inputHandler="updateField"
           />
-          <PluginProperty
-            id="user"
-            label="User"
-            :value="formValues.user"
-            name="user"
-            :show="showCredentials"
-            :inputHandler="updateField"
-            :errorMsg="errors['user']"
-            :validator="v(required)"
-          />
-          <PluginProperty
-            id="password"
-            label="Password"
-            inputType="password"
-            :errorMsg="errors['password']"
-            :value="formValues.password"
-            name="password"
-            :show="showCredentials"
-            :inputHandler="updateField"
-            :validator="v(required)"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="user"
+                label="User"
+                :value="formValues.user"
+                name="user"
+                :show="showCredentials"
+                :inputHandler="updateField"
+                :errorMsg="errors['user']"
+                :validator="v(required)"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="password"
+                label="Password"
+                inputType="password"
+                :errorMsg="errors['password']"
+                :value="formValues.password"
+                name="password"
+                :show="showCredentials"
+                :inputHandler="updateField"
+                :validator="v(required)"
+              />
+            </div>
+          </div>
           <PluginProperty
             id="url"
             label="Url"
@@ -99,49 +111,60 @@
           />
 
           <!-- email custom properties start -->
-          <PluginProperty
-            id="host"
-            label="Host"
-            :errorMsg="errors['host']"
-            :value="formValues.host"
-            description="Mandatory: SMTP host name (e.g. smtp.gmail.com)"
-            :show="isEmail"
-            :inputHandler="updateField"
-            :validator="v(required)"
-          />
-          <PluginProperty
-            id="port"
-            label="Port"
-            inputType="number"
-            :errorMsg="errors['port']"
-            :value="formValues.port"
-            description="Mandatory: SMTP server port (e.g. 587)"
-            :show="isEmail"
-            :inputHandler="updateField"
-            :validator="v(required)"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="host"
+                label="Host"
+                :errorMsg="errors['host']"
+                :value="formValues.host"
+                description="Mandatory: SMTP host name (e.g. smtp.gmail.com)"
+                :show="isEmail"
+                :inputHandler="updateField"
+                :validator="v(required)"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="port"
+                label="Port"
+                inputType="number"
+                :errorMsg="errors['port']"
+                :value="formValues.port"
+                description="Mandatory: SMTP server port (e.g. 587)"
+                :show="isEmail"
+                :inputHandler="updateField"
+                :validator="v(required)"
+              />
+            </div>
+          </div>
 
-          <PluginProperty
-            id="sender"
-            label="Sender"
-            :value="formValues.sender"
-            description="The email address to use as a sender"
-            :errorMsg="errors['sender']"
-            :show="isEmail"
-            :inputHandler="updateField"
-            :validator="v(email)"
-          />
-
-          <PluginProperty
-            id="recipients"
-            label="Recipients"
-            :value="formValues.recipients | toString"
-            description="Mandatory: comma separated list of recipients"
-            :errorMsg="errors['recipients']"
-            :show="isEmail"
-            :inputHandler="updateCollectionField"
-            :validator="v(recipients)"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="sender"
+                label="Sender"
+                :value="formValues.sender"
+                description="The email address to use as a sender"
+                :errorMsg="errors['sender']"
+                :show="isEmail"
+                :inputHandler="updateField"
+                :validator="v(email)"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="recipients"
+                label="Recipients"
+                :value="formValues.recipients | toString"
+                description="Mandatory: comma separated list of recipients"
+                :errorMsg="errors['recipients']"
+                :show="isEmail"
+                :inputHandler="updateCollectionField"
+                :validator="v(recipients)"
+              />
+            </div>
+          </div>
 
           <PluginCheckboxProperty
             id="useMX"
@@ -153,17 +176,31 @@
           <!-- email custom properties end -->
 
           <!-- jira custom properties start -->
-          <PluginProperty
-            id="projectKey"
-            label="Project Key"
-            name="project_key"
-            :value="formValues.project_key"
-            :errorMsg="errors['project_key']"
-            :description="'Mandatory. Specify the JIRA project key'"
-            :show="isJira"
-            :inputHandler="updateField"
-            :validator="v(required)"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="projectKey"
+                label="Project Key"
+                name="project_key"
+                :value="formValues.project_key"
+                :errorMsg="errors['project_key']"
+                :description="'Mandatory. Specify the JIRA project key'"
+                :show="isJira"
+                :inputHandler="updateField"
+                :validator="v(required)"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="board"
+                label="Board"
+                :value="formValues.board"
+                description="Optional. Specify the Jira board name to open tickets on"
+                :show="isJira"
+                :inputHandler="updateField"
+              />
+            </div>
+          </div>
 
           <PluginCheckboxProperty
             id="tlsVerify"
@@ -174,31 +211,29 @@
             :inputHandler="updateField"
           />
 
-          <PluginProperty
-            id="board"
-            label="Board"
-            :value="formValues.board"
-            description="Optional. Specify the Jira board name to open tickets on"
-            :show="isJira"
-            :inputHandler="updateField"
-          />
 
-          <PluginProperty
-            id="fixVersions"
-            label="Fix Versions"
-            :value="formValues.fixVersions | toString"
-            description="Optional, specify comma separated list of Fix versions to add to Ticket"
-            :show="isJira"
-            :inputHandler="updateCollectionField"
-          />
-          <PluginProperty
-            id="affectsVersions"
-            label="Affects Versions"
-            :value="formValues.affectsVersions | toString"
-            description="Optional, specify comma separated list of Affects versions to add to Ticket"
-            :show="isJira"
-            :inputHandler="updateCollectionField"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="fixVersions"
+                label="Fix Versions"
+                :value="formValues.fixVersions | toString"
+                description="Optional, specify comma separated list of Fix versions to add to Ticket"
+                :show="isJira"
+                :inputHandler="updateCollectionField"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="affectsVersions"
+                label="Affects Versions"
+                :value="formValues.affectsVersions | toString"
+                description="Optional, specify comma separated list of Affects versions to add to Ticket"
+                :show="isJira"
+                :inputHandler="updateCollectionField"
+              />
+            </div>
+          </div>
 
           <PluginProperty
             id="labels"
@@ -208,39 +243,67 @@
             :show="isJira"
             :inputHandler="updateCollectionField"
           />
-          <PluginProperty
-            id="issuetype"
-            label="Issue Type"
-            :value="formValues.issuetype"
-            description="Optional. Specify the issue type to open (Bug, Task, etc.). Default is Task"
-            :show="isJira"
-            :inputHandler="updateField"
-          />
-          <PluginProperty
-            id="priority"
-            label="Priority"
-            :value="formValues.priority"
-            description="Optional. Specify the issues severity. Default is High"
-            :show="isJira"
-            :inputHandler="updateField"
-          />
-          <PluginProperty
-            id="assignee"
-            label="Assignee"
-            :value="formValues.assignee | toString"
-            :description="jiraAssigneeDescription"
-            :show="isJira"
-            :inputHandler="updateCollectionField"
-          />
-          <PluginProperty
-            id="sprint"
-            label="Sprint"
-            :value="formValues.sprint"
-            description="Optional Sprint name, e.g., '3.5 Sprint 8'"
-            :show="isJira"
-            :inputHandler="updateField"
-          />
-          <h5 v-show="isJira">Jira custom fields</h5>
+
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="issuetype"
+                label="Issue Type"
+                :value="formValues.issuetype"
+                description="Optional. Specify the issue type to open (Bug, Task, etc.). Default is Task"
+                :show="isJira"
+                :inputHandler="updateField"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="priority"
+                label="Priority"
+                :value="formValues.priority"
+                description="Optional. Specify the issues severity. Default is High"
+                :show="isJira"
+                :inputHandler="updateField"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="assignee"
+                label="Assignee"
+                :value="formValues.assignee | toString"
+                :description="jiraAssigneeDescription"
+                :show="isJira"
+                :inputHandler="updateCollectionField"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="sprint"
+                label="Sprint"
+                :value="formValues.sprint"
+                description="Optional Sprint name, e.g., '3.5 Sprint 8'"
+                :show="isJira"
+                :inputHandler="updateField"
+              />
+            </div>
+          </div>
+          <div class="row mb-3" v-show="isJira">
+            <div class="col-sm-7">
+              <h5>Jira custom fields</h5>
+            </div>
+            <div class="col-sm-4 ">
+                <b-form-input
+                  id="cf-adder"
+                  v-model="addedControl"
+                  type="text"
+                  placeholder="Add new custom field"
+                ></b-form-input>
+            </div>
+            <div class="col-sm-1 text-right">
+               <b-button variant="primary" @click="addCf" title="Add field">Add</b-button>
+            </div>
+          </div>
 
           <b-row class="form-group" v-for="(cfValue, cfName) in unknowns" v-bind:key="cfName" :show="isJira">
             <b-col sm="2">
@@ -249,64 +312,66 @@
             <b-col sm="9">
               <b-form-input :id="'cf-' + cfName" :name="cfName" v-model="unknowns[cfName]"></b-form-input>
             </b-col>
-            <b-col sm="1" class="pr-2">
-              <b-button variant="primary" @click="removeCf(cfName)" ><span aria-hidden="true">&times;</span></b-button>
+            <b-col sm="1" class="text-right">
+              <b-button variant="primary" title="Remove field" @click="removeCf(cfName)" ><span aria-hidden="true">&times;</span></b-button>
             </b-col>
           </b-row>
 
-          <div class="form-group mt-3 row" v-show="isJira">
-            <b-form-input
-              id="cf-adder"
-              v-model="addedControl"
-              class="col-sm-3 mr-2 ml-2"
-              type="text"
-              placeholder="Add new custom field"
-            ></b-form-input>
-            <b-button variant="primary" @click="addCf">Add</b-button>
-          </div>
 
           <!-- jira custom properties end -->
           <!-- serviceNow custom properties start -->
-          <PluginProperty
-            id="instance"
-            label="Instance"
-            :value="formValues.instance"
-            description="Mandatory. Name of ServiceNow  or Instance"
-            :errorMsg="errors['instance']"
-            :show="isServiceNow"
-            :inputHandler="updateField"
-            :validator="v(required)"
-          />
-          <PluginProperty
-            id="board"
-            label="Board"
-            :value="formValues.board"
-            description="Specify the ServiceNow board name to open tickets on. Default is incident"
-            :show="isServiceNow"
-            :inputHandler="updateField"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="instance"
+                label="Instance"
+                :value="formValues.instance"
+                description="Mandatory. Name of ServiceNow  or Instance"
+                :errorMsg="errors['instance']"
+                :show="isServiceNow"
+                :inputHandler="updateField"
+                :validator="v(required)"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="board"
+                label="Board"
+                :value="formValues.board"
+                description="Specify the ServiceNow board name to open tickets on. Default is incident"
+                :show="isServiceNow"
+                :inputHandler="updateField"
+              />
+            </div>
+          </div>
           <!-- serviceNow custom properties end -->
           <!-- splunk custom properties start -->
-          <PluginProperty
-            id="token"
-            label="Token"
-            :value="formValues.token"
-            :errorMsg="errors['token']"
-            description="Mandatory. a HTTP Event Collector Token"
-            :show="isSplunk"
-            :inputHandler="updateField"
-            :validator="v(required)"
-          />
-          <PluginProperty
-            id="sizeLimit"
-            label="Size Limit"
-            :value="formValues.SizeLimit"
-            inputType="number"
-            :name="'SizeLimit'"
-            description="Optional. Maximum scan length, in bytes. Default: 10000"
-            :show="isSplunk"
-            :inputHandler="updateField"
-          />
+          <div class="row">
+            <div class="col">
+              <PluginProperty
+                id="token"
+                label="Token"
+                :value="formValues.token"
+                :errorMsg="errors['token']"
+                description="Mandatory. a HTTP Event Collector Token"
+                :show="isSplunk"
+                :inputHandler="updateField"
+                :validator="v(required)"
+              />
+            </div>
+            <div class="col">
+              <PluginProperty
+                id="sizeLimit"
+                label="Size Limit"
+                :value="formValues.SizeLimit"
+                inputType="number"
+                :name="'SizeLimit'"
+                description="Optional. Maximum scan length, in bytes. Default: 10000"
+                :show="isSplunk"
+                :inputHandler="updateField"
+              />
+            </div>
+          </div>
           <!-- splunk custom properties end -->
         </div>
       </form>
