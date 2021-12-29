f5cefcbae53e65c78dfc48451f4fd2b2fb02771a
DmitriyLewen <dmitriy.lewen@smartforce.io>
2021-12-29 16:54:43 +0600

    test: added test for errors to getCfgCacheSource

--- dbservice/postgresdb/cfgcachesource_test.go
+++ dbservice/postgresdb/cfgcachesource_test.go
@@ -53,32 +53,35 @@ func TestGetCfgCacheSourceErrors(t *testing.T) {
 		{"select error", errors.New("select error"), "", "error getting cfg cache source: select error"},
 	}
 	for _, test := range tests {
-		savedPsqlConnect := psqlConnect
-		psqlConnect = func(connectUrl string) (*sqlx.DB, error) {
-			db, mock, err := sqlxmock.Newx()
-			if err != nil {
-				log.Println("failed to open sqlmock database:", err)
+		t.Run(test.name, func(t *testing.T) {
+			savedPsqlConnect := psqlConnect
+			psqlConnect = func(connectUrl string) (*sqlx.DB, error) {
+				db, mock, err := sqlxmock.Newx()
+				if err != nil {
+					log.Println("failed to open sqlmock database:", err)
+				}
+				mock.ExpectQuery("SELECT").WillReturnError(test.err)
+				return db, err
 			}
-			mock.ExpectQuery("SELECT").WillReturnError(test.err)
-			return db, err
-		}
-		savedInsertCfgCacheSource := insertCfgCacheSource
-		insertCfgCacheSource = func(db *sqlx.DB, tenantName, cfgFile string) error { return nil }
-		defer func() {
-			psqlConnect = savedPsqlConnect
-			insertCfgCacheSource = savedInsertCfgCacheSource
-		}()
+			savedInsertCfgCacheSource := insertCfgCacheSource
+			insertCfgCacheSource = func(db *sqlx.DB, tenantName, cfgFile string) error { return nil }
+			defer func() {
+				psqlConnect = savedPsqlConnect
+				insertCfgCacheSource = savedInsertCfgCacheSource
+			}()
 
-		cfg, err := GetCfgCacheSource(db)
-		if test.expectedErr != "" || err != nil {
-			if err.Error() != test.expectedErr {
-				t.Errorf("Unexpected err, expected: %v, got: %v", test.expectedErr, err)
+			cfg, err := GetCfgCacheSource(db)
+			if test.expectedErr != "" || err != nil {
+				if err.Error() != test.expectedErr {
+					t.Errorf("Unexpected err, expected: %v, got: %v", test.expectedErr, err)
+				}
 			}
-		}
 
-		if cfg != test.expectedCfg {
-			t.Errorf("Bad cfg, expected: %s, got: %s", test.expectedCfg, cfg)
-		}
+			if cfg != test.expectedCfg {
+				t.Errorf("Bad cfg, expected: %s, got: %s", test.expectedCfg, cfg)
+			}
+		})
+
 	}
 
 }
