f83265c26e44f89387ba75f48290bfb1f4f54394
Simar <simar@linux.com>
2022-02-11 16:50:20 -0800

    chore: Refactor kubernetes_test.go
    Signed-off-by: Simar <simar@linux.com>

--- outputs/kubernetes_test.go
+++ outputs/kubernetes_test.go
@@ -16,13 +16,42 @@ import (
 )
 
 func TestKubernetesClient_Send(t *testing.T) {
-	t.Run("happy path", func(t *testing.T) {
+	testCases := []struct {
+		name           string
+		reactorFunc    func(k8stesting.Action) (bool, runtime.Object, error)
+		expectedLabels map[string]string
+	}{
+		{
+			name: "happy path, labels are added",
+			expectedLabels: map[string]string{
+				"app": "nginx",
+				"foo": "bar",
+			},
+		},
+		{
+			name: "sad path, unable to add label",
+			reactorFunc: func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+				return true, nil, fmt.Errorf("failed to update label")
+			},
+			expectedLabels: map[string]string{
+				"app": "nginx",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
 		k := KubernetesClient{
 			clientset: fake.NewSimpleClientset(),
 			KubeLabels: map[string]string{
 				"foo": "bar",
 			},
 			KubeLabelSelector: "app=nginx",
+		}
+
+		if tc.reactorFunc != nil {
+			k.clientset.CoreV1().(*fake2.FakeCoreV1).Fake.PrependReactor("patch", "pods", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+				return true, nil, fmt.Errorf("failed to update label")
+			})
 		}
 
 		pod := &v1.Pod{
@@ -38,43 +67,10 @@ func TestKubernetesClient_Send(t *testing.T) {
 		}
 
 		_, err := k.clientset.CoreV1().Pods("testing").Create(context.TODO(), pod, metav1.CreateOptions{})
-		require.NoError(t, err)
-		require.NoError(t, k.Send(map[string]string{"foo": "bar"}))
+		require.NoError(t, err, tc.name)
+		require.NoError(t, k.Send(map[string]string{"foo": "bar"}), tc.name)
 
 		pods, _ := k.clientset.CoreV1().Pods("testing").Get(context.TODO(), "test-pod", metav1.GetOptions{})
-		assert.Equal(t, map[string]string{"app": "nginx", "foo": "bar"}, pods.Labels)
-	})
-
-	t.Run("sad path, unable to apply label", func(t *testing.T) {
-		k := KubernetesClient{
-			clientset: fake.NewSimpleClientset(),
-			KubeLabels: map[string]string{
-				"foo": "bar",
-			},
-			KubeLabelSelector: "app=nginx",
-		}
-		k.clientset.CoreV1().(*fake2.FakeCoreV1).Fake.PrependReactor("patch", "pods", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-			return true, nil, fmt.Errorf("failed to update label")
-		})
-
-		pod := &v1.Pod{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "Pod",
-				APIVersion: "v1",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test-pod",
-				Namespace: "testing",
-				Labels:    map[string]string{"app": "nginx"},
-			},
-		}
-
-		_, err := k.clientset.CoreV1().Pods("testing").Create(context.TODO(), pod, metav1.CreateOptions{})
-		require.NoError(t, err)
-
-		require.NoError(t, k.Send(map[string]string{"foo": "bar"}))
-
-		pods, _ := k.clientset.CoreV1().Pods("testing").Get(context.TODO(), "test-pod", metav1.GetOptions{})
-		assert.Equal(t, map[string]string{"app": "nginx"}, pods.Labels)
-	})
+		assert.Equal(t, tc.expectedLabels, pods.Labels, tc.name)
+	}
 }
