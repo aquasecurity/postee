0a1321cf8a62ee2d4f324a10a58214f1c0b2e583
simar7 <1254783+simar7@users.noreply.github.com>
2022-08-16 14:19:18 -0700

    feat(ui): Improved Messaging for UI elements. (#441)
    Signed-off-by: Simar <simar@linux.com>
    
    Signed-off-by: Simar <simar@linux.com>

--- ui/frontend/src/components/Routes.vue
+++ ui/frontend/src/components/Routes.vue
@@ -3,6 +3,8 @@
     <div class="row justify-content-end pb-3 pr-3">
         <router-link id="add-route" :to="{name: 'add-route'}" class="btn btn-primary">Add Route</router-link>
     </div>
+  <div class="card-header"><h3>Routes</h3></div>
+  <div class="card-body">A route is used to control message flows. Each route includes the input message condition, the template that should be used to format the message, and the action(s) that the message should be delivered to.</div>
     <div class="row row-cols-1 row-cols-md-3">
         <RouteCard v-for="(route, index) in routes" :key="'route-' + index" :name="route.name" :actionCnt="actionCnt(route.actions)"/>
     </div>
